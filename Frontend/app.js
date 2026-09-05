e0ade2e3a643e3ef8fa18c60588ff1f2f825fec0
Dileep Yadav <dy695354612@gmail.com>
2026-09-05 15:31:37 +0530

    miscellians

---
+++ Frontend/app.js
@@ -0,0 +1,17 @@
+const stu1 = {
+    name: "adam",
+    age: 25,
+    marks: 95,
+};
+
+const stu2 = {
+    name: "eve",
+    age: 25,
+    marks: 99,
+};
+
+const stu3 = {
+    name: "casey",
+    age: 23,
+    marks: 85,
+};
